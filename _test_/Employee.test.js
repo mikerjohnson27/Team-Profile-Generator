307eafb745a378536b62e4a4296af3facdcfbeae
Mike Johnson <mikerjohnson27unh@gmail.com>
2022-05-26 17:27:23 -0400

    adding JS to Employee.test.js file

---
+++ _test_/Employee.test.js
@@ -0,0 +1,7 @@
+discribe("Employee", () => {
+    discribe("identification", () => {
+        it("needs to have a name," + "a employee ID and email address", () => {
+            const employee = new Employee ("firstName lastName", [1512], [])
+        })
+    })
+})
