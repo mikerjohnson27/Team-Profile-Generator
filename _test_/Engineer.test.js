68ca67514dde339a846827db6e564ff99819e8fb
Mike Johnson <mikerjohnson27unh@gmail.com>
2022-05-27 19:57:08 -0400

    adding to test files.

---
+++ _test_/Engineer.test.js
@@ -0,0 +1,11 @@
+discribe("Employee", () => {
+    discribe("identification", () => {
+        it("needs to have a name," + "a employee ID and email address", () => {
+            const employee = new Employee ("first last", 15422, "first.last@mrjtechinc.com")
+            expect(employee.getName()).toEqual("first last");
+            expect(employee.getID()).toEqual(15422);
+            expect(employee.getEmail()).toEqual("first.last@mrjtechinc.com");
+            expect(employee.getRole()).toEqual("Employee");
+        })
+    })
+})
