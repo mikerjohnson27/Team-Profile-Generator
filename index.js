52378d0e6a262bd117f6f0909747ed3a504dd91d
Mike Johnson <mikerjohnson27unh@gmail.com>
2022-05-30 16:00:50 -0400

    ading questions, classes and html generations template

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
+//HTML Generation Template File
+const gernerateHTML = require('./src/HTMLtemplate.js')
+//Express dependency
 const express = require('express');
 const app = express();
+//FS is requires for file generation
+const fs = require('fs')
+
+//All awnsers will be pushed here
+const teamArray = []
+
+//Questions to add by the type of employee
+const typeOfEmployee = {
+
+};
+
+
+//questions if manager is selected in type of employee questions
+const managerQuestions = {
+
+};
+
+//questions if employee is selected in type of employee questions
+const employeeQuestions = {
+
+};
+
+//questions if engineer is selected in type of employee questions
+const engineerQuestions = {
+
+};
